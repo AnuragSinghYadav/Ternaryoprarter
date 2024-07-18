882950474a1902510314892d38c1dbb0f8b797aa
Anurag Singh Yadav <117581504+AnuragSinghYadav@users.noreply.github.com>
2024-07-18 13:24:51 +0530

    Add files via upload
    it is in python simple ternary opeater

---
+++ Ternaryopeter.py
@@ -0,0 +1,6 @@
+#TERNARY OPEARTER syntax:
+#variable = value 1 if condition else value 2
+number=int(input("Enter a no:"))
+output = "Even" if number % 2 == 0 else "Odd"
+#print("output is ",output)
+print("output is","Even" if number % 2 == 0 else "Odd")
